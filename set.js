0196b223838ede2e6d890fee7c3fd2704dc8e99b
Damanage <derongar@gmail.com>
2018-04-19 17:35:11 +0300

    inersection done

--- set.js
+++ set.js
@@ -74,7 +74,7 @@ class Set {
         let firstSet = array1.values.concat(this.values),
             megaSet = new Set;
         megaSet.add(firstSet);
-        return megaSet
+        return megaSet;
     }
 
     // /**
@@ -82,7 +82,17 @@ class Set {
     //  * @param {Set} set
     //  * @returns {Set}
     //  */
-    // intersection() {}
+    intersection(param) {
+        // let fullSet = param.values.concat(this.values),
+        let megaSet = new Set;
+        param.values.forEach((wookie)=>{
+            if(this.has(wookie)){
+                megaSet._box.push(wookie)
+                
+            }
+        })
+        return megaSet;
+    }
 
     // /**
     //  * Возращает сет состоящий из элементов присутствующих в первом сете, и отсутствующих во втором
@@ -101,15 +111,20 @@ class Set {
 
 joe = new Set;
 sam = new Set;
-joe.add('penis')
+
+joe.add('t-rex')
 joe.add('dickus')
 joe.add('salmon')
+joe.add('мишаня')
 joe.add('lama')
+
+
 sam.add('duck')
 sam.add('spider-duck')
 sam.add('t-rex')
 sam.add('мишаня')
-jack = sam.union(joe);
-willy = joe.union(sam);
+sam.add('salmon')
 
+
+willy = sam.intersection(joe)
 console.log(willy)
